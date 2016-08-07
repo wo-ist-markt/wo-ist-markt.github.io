2a92c2cc85a0ca50bae6cee0cbef2966df9c20e1
Torf <mail@torf.cc>
2016-08-07 19:03:15 +0200

    `npm run aggregate` now also aggregates the cities' metadata.
    The aggregated metadata is stored in `build/cities.json`. It contains
    the content of `cities/cities.json` plus, for each city, the metadata
    from the city's GeoJSON.

--- dev/aggregate.js
+++ dev/aggregate.js
@@ -15,42 +15,45 @@ var CITIES_DIR = path.join(__dirname, '..', 'cities');
 var CITIES_FILE = path.join(CITIES_DIR, 'cities.json');
 
 var BUILD_DIR = path.join(__dirname, '..', 'build');
+var AGGREGATED_CITIES_FILE = path.join(BUILD_DIR, 'cities.json');
 var AGGREGATED_MARKETS_FILE = path.join(BUILD_DIR, 'markets.json');
 
 
 /*
- * Merge two or more GeoJSON objects.
+ * Merge the features of GeoJSON objects.
  *
- * Takes a `target` GeoJSON object and one or more source GeoJSON objects and
- * merges the features from the source objects into the target object. Note
- * that other GeoJSON data from the sources (e.g. `metadata`) is *not* copied.
+ * Takes a `target` GeoJSON object and a list `sources` of one or more source
+ * GeoJSON objects and merges the features from the source objects into the
+ * target object. Note that other GeoJSON data from the sources is *not*
+ * copied.
  *
  * The target object is returned.
  */
-function mergeGeoJSONFeatures(target /* , args */) {
+function mergeFeatures(target, sources) {
     if (target.features === undefined) {
         target.features = [];
     }
-    for (let i = 1; i < arguments.length; i++) {
-        Array.prototype.push.apply(target.features, arguments[i].features);
+    for (let source of sources) {
+        Array.prototype.push.apply(target.features, source.features);
     }
     return target;
 }
 
 
 /*
- * Aggregate markets data from all cities.
+ * Aggregate data from all cities.
  *
  * Loads each city's GeoJSON data and merges all GeoJSON features into a single
- * file (`AGGREGATED_MARKETS_FILE`).
+ * file (`AGGREGATED_MARKETS_FILE`). All metadata is merged into a separate
+ * file (`AGGREGATED_CITIES_FILE`).
  *
- * Returns a promise that resolves to the output filename once all data has
+ * Returns two promises that resolve to the output filenames once all data has
  * been aggregated and written to disk.
  */
-function aggregateMarketsData() {
+function aggregate() {
     return utils.loadJSON(CITIES_FILE)
-        .then(json => {
-            let aggregated = {
+        .then(citiesJSON => {
+            let marketsJSON = {
                 "crs": {
                     "properties": {
                         "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
@@ -61,22 +64,34 @@ function aggregateMarketsData() {
                 "features": []
             };
             let promises = [];
-            for (let city in json) {
-                let p = utils.loadJSON(path.join(CITIES_DIR, city + '.json'))
-                    .then(cityJSON => mergeGeoJSONFeatures(aggregated, cityJSON));
-                promises.push(p);
+            for (let city in citiesJSON) {
+                let p = utils.loadJSON(path.join(CITIES_DIR, city + '.json'));
+                promises.push(p.then(cityJSON => {
+                    for (let key in cityJSON.metadata) {
+                        citiesJSON[city][key] = cityJSON.metadata[key];
+                    }
+                }));
+                promises.push(p.then(cityJSON =>
+                    mergeFeatures(marketsJSON, [cityJSON])));
             }
             promises.push(utils.ensureDir(BUILD_DIR));
-            return Promise.all(promises)
-                .then(values => utils.saveJSON(aggregated, AGGREGATED_MARKETS_FILE));
+            let p = Promise.all(promises);
+            return [
+                p.then(_ => utils.saveJSON(citiesJSON, AGGREGATED_CITIES_FILE)),
+                p.then(_ => utils.saveJSON(marketsJSON, AGGREGATED_MARKETS_FILE))
+            ];
         });
 }
 
 
 if (require.main === module) {
-    aggregateMarketsData()
-        .then(filename => console.log('Markets data has been aggregated to ' +
-                                      filename))
+    aggregate()
+        .then(_ => {
+            console.log('Markets data has been aggregated to ' +
+                        AGGREGATED_MARKETS_FILE);
+            console.log('Cities metadata has been aggregated to ' +
+                        AGGREGATED_CITIES_FILE);
+        })
         .catch(err => console.error(err));
 }
 
