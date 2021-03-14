8f347eb684010e3d5c9e737794eadd3ae81934e0
Knut Hühne <knut@k-nut.eu>
2021-03-14 13:51:27 +0100

    Exit on failure on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euf -o pipefail
+
 if [[ $TRAVIS_PULL_REQUEST == "false" &&  $TRAVIS_BRANCH == "master" ]]; then
   # Decrypt the SSH key that is used for authentication at our server
   openssl aes-256-cbc -K $encrypted_02f86fe65d47_key -iv $encrypted_02f86fe65d47_iv -in markt_deploy_id_rsa.enc -out markt_deploy_id_rsa -d
