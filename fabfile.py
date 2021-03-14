de98070bf447c89b8f0921946f80eb30e9f4bfea
Knut Hühne <knut@k-nut.eu>
2021-03-14 13:52:21 +0100

    Update to Fabric 2 (fabric 1 does not support py3)

--- fabfile.py
+++ fabfile.py
@@ -8,43 +8,51 @@
     It is also possible to use the `list_revisions` command to see which
     revisions are published to the server.
  """
-from fabric.api import run, env, local, put, cd
+from fabric import task
 
 BASE_PATH = '/home/deploy/versions'
 
-def deploy():
-    current_revision = local('git rev-parse HEAD', capture=True)
-    create_artifact(current_revision)
-    upload_artifact(current_revision)
-    make_active(current_revision)
 
-def create_artifact(current_revision):
+@task
+def deploy(c):
+    current_revision = c.local('git rev-parse HEAD', warn=True).stdout.strip()
+    create_artifact(c, current_revision)
+    upload_artifact(c, current_revision)
+    make_active(c, current_revision)
+
+
+def create_artifact(c, current_revision):
     """ compress all files into a .tar.gz archive for uploading """
     archive_path = '/tmp/{revision}.tar.gz'.format(revision=current_revision)
-    local('tar -czf {archive_path} --exclude=.git *'.format(archive_path=archive_path))
+    c.local('tar -czf {archive_path} --exclude=.git *'.format(archive_path=archive_path))
 
-def upload_artifact(revision):
+
+def upload_artifact(c, revision):
     """ upload the archive to the server and extract it """
     # we upload the file from the local /tmp to the remote /tmp dir
     tmp_path = '/tmp/{revision}.tar.gz'.format(revision=revision)
-    put(tmp_path, tmp_path)
+    c.put(tmp_path, tmp_path)
 
     destination_path = '{base}/{revision}'.format(base=BASE_PATH,
                                                   revision=revision)
-    untar(tmp_path, destination_path)
+    untar(c, tmp_path, destination_path)
 
     # remove both local and remote archives
-    run('rm {}'.format(tmp_path))
-    local('rm {}'.format(tmp_path))
+    c.run('rm {}'.format(tmp_path))
+    c.local('rm {}'.format(tmp_path))
 
-def untar(source_path, destination_path):
-    run('mkdir -p %s' % destination_path)
-    run('tar xfz %s -C %s' % (source_path, destination_path))
 
-def make_active(revision):
+def untar(c, source_path, destination_path):
+    c.run('mkdir -p %s' % destination_path)
+    c.run('tar xfz %s -C %s' % (source_path, destination_path))
+
+
+def make_active(c, revision):
     """ change the `newest` symlink to point to this revision """
-    run('ln -sfn {base}/{revision}/ {base}/newest'.format(base=BASE_PATH,
-                                                          revision=revision))
+    c.run('ln -sfn {base}/{revision}/ {base}/newest'.format(base=BASE_PATH,
+                                                            revision=revision))
 
-def list_versions():
-    run('ls -l {base}'.format(base=BASE_PATH))
+
+@task
+def list_versions(c):
+    c.run('ls -l {base}'.format(base=BASE_PATH))
