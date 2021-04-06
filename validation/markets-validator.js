c0ca9a269b12d6c7b9d839018e2950307c2c4419
Jannis R <mail@jannisr.de>
2021-04-06 13:42:20 +0200

    URL validation: send User-Agent, destroy() response

--- validation/markets-validator.js
+++ validation/markets-validator.js
@@ -537,6 +537,10 @@ function MetadataValidator(metadata, cityName) {
         request = request.request(url, {
             method: 'HEAD',
             timeout: 10000, // 10 seconds
+            headers: {
+                'User-Agent': REPO_URL,
+                'Accept': '*/*',
+            },
         }, function(response) {
             if (response.statusCode >= 300 && response.statusCode < 400) {
                 asyncWarnings.push(new HttpRedirectStatusIssue(cityName, response.statusCode, response.headers.location));
@@ -545,10 +549,7 @@ function MetadataValidator(metadata, cityName) {
             }
 
             // Cleaning up http request and response
-            // "However, if you add a 'response' event handler, then you
-            //  must consume the data from the response object" NodeJS docs
-            response.on('data', function() {});
-            response.on('end', function() {});
+            response.destroy();
         });
         request.on('error', function(error) {
             asyncWarnings.push(new HttpRequestErrorIssue(cityName, error));
