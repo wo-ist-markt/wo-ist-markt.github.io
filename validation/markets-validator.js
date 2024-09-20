000dce67595967980b213867ff849d7ec413bb83
Martin Weis <Martin.Weis.newsadress@gmx.de>
2024-09-20 08:37:33 +0200

    validation warnings count reduced

--- validation/markets-validator.js
+++ validation/markets-validator.js
@@ -52,7 +52,7 @@ var MIN_LONGITUDE = -180.0;
  * is impossible for us to resolve the issue.
  * The aim is to keep this count as low as possible.
  */
-var ACCEPTABLE_WARNINGS_COUNT = 3; // Marl, Hilden, Potsdam
+var ACCEPTABLE_WARNINGS_COUNT = 1; // Mallorca:403(user-agent)
 
 var exitCode = 0;
 
