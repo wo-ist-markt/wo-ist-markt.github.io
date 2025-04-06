49fb5675c2998c607090f6939e8d1d3b91d01abe
Tobias Preuss <tobias.preuss@googlemail.com>
2025-04-06 12:55:43 +0200

    Update data source URL for Wiesbaden.

--- validation/markets-validator.js
+++ validation/markets-validator.js
@@ -52,7 +52,7 @@ var MIN_LONGITUDE = -180.0;
  * is impossible for us to resolve the issue.
  * The aim is to keep this count as low as possible.
  */
-var ACCEPTABLE_WARNINGS_COUNT = 2; // Marl IPv6, Chemnitz user agent
+var ACCEPTABLE_WARNINGS_COUNT = 2; // Bremen, Chemnitz user agent
 
 var exitCode = 0;
 
