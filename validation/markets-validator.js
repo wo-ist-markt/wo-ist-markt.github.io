6c2f7e833604a877e5d1babbf20176c625167428
Tobias Preuss <tobias.preuss@googlemail.com>
2025-06-20 18:33:35 +0200

    Temporary exclude unresponsive Rostock and Stralsund.

--- validation/markets-validator.js
+++ validation/markets-validator.js
@@ -52,7 +52,7 @@ var MIN_LONGITUDE = -180.0;
  * is impossible for us to resolve the issue.
  * The aim is to keep this count as low as possible.
  */
-var ACCEPTABLE_WARNINGS_COUNT = 2; // Bremen, Chemnitz user agent
+var ACCEPTABLE_WARNINGS_COUNT = 4; // Rostock, Stralsund, Bremen, Chemnitz user agent
 
 var exitCode = 0;
 
